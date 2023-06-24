cbc6b2b2e2f53796ecb28044c104553502e36f18
rihilrutilant <rihilrutilant@gmail.com>
2023-06-24 11:51:51 +0530

    sdf

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Component/Home";
+import Login from "./Component/Login";
+import CustomerService from "./Component/CustomerService";
+import Plans from "./Component/Plans";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/customerService" element={<CustomerService />} />
+        <Route path="/plans" element={<Plans />} />
+      </Routes>
+    </BrowserRouter >
   );
 }
 
