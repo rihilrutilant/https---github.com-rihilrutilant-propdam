0501c2f1c5d068003b733378af7cfe048b50a92d
yashviSachapara <129357358+yashviSachapara@users.noreply.github.com>
2023-06-26 11:38:13 +0530

    26 june-2

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -9,93 +9,142 @@ import p1 from "../Assets/R.png";
 import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <div className="main-f">
-      <div className="inner-main-fpart">
-        <img className="logo" src={p1} alt="" />
-        <div className="m-in">
-          <h3 className="in-text">FOLLOW US :</h3>
-          <div className="in-fb">
-            <Link>
-              {" "}
-              <button className="fb">
-                <FaFacebookF />
-              </button>{" "}
-            </Link>
+    <>
+      <section>
+        <div className="main-f">
+          <div className="footer_top">
+            <div className="container-fluid">
+              <div className="inner-main-fpart">
+                <img className="p_logo" src={p1} alt="" />
+                <div className="m-in">
+                  <h3 className="in-text">FOLLOW US :</h3>
+                  <div className="in-fb">
+                    <Link>
+                      {" "}
+                      <button className="fb">
+                        <FaFacebookF />
+                      </button>{" "}
+                    </Link>
+                  </div>
+                  <div className="in-insta">
+                    <Link>
+                      <button className="insta">
+                        <FaInstagram />
+                      </button>
+                    </Link>
+                  </div>
+                  <div className="in-yt">
+                    <Link>
+                      <button className="yt">
+                        <FaYoutube />
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="in-insta">
-            <Link>
-              <button className="insta">
-                <FaInstagram />
-              </button>
-            </Link>
+          {/* footer inner part */}
+          <div className="inner-info container-fluid">
+            <div className="Propdam-part">
+              <h3 className="p-h3">Propdam</h3>
+              <Link>
+                <p>Mobile Apps</p>
+              </Link>
+              <Link>
+                <p>Our Services</p>
+              </Link>
+              <Link>
+                <p>Price Trends</p>
+              </Link>
+              <Link>
+                <p>Builder in India</p>
+              </Link>
+              <Link>
+                <p>Area Converter</p>
+              </Link>
+              <Link>
+                <p>Articles</p>
+              </Link>
+              <Link>
+                <p>Customer Services</p>
+              </Link>
+              <Link>
+                <p>Sitemap</p>
+              </Link>
+            </div>
+            <div className="Company-part">
+              <h3 className="p-h3">Company</h3>
+              <Link>
+                <p>About Us</p>
+              </Link>
+              <Link>
+                <p>Contact Us</p>
+              </Link>
+              <Link>
+                <p>Carrers with us</p>
+              </Link>
+              <Link>
+                <p>Terms & Conditions</p>
+              </Link>
+              <Link>
+                <p>Request Info</p>
+              </Link>
+              <Link>
+                <p>Feedback</p>
+              </Link>
+              <Link>
+                <p>Report a problem</p>
+              </Link>
+              <Link>
+                <p>Testimonials</p>
+              </Link>
+              <Link>
+                <p>Privacy Policy</p>
+              </Link>
+              <Link>
+                <p>Summons/Notices</p>
+              </Link>
+              <Link>
+                <p>Grievances</p>
+              </Link>
+              <Link>
+                <p>Safety Guide</p>
+              </Link>
+            </div>
+            <div className="Contact-us-part">
+              <h3 className="p-h3">Contact Us</h3>
+              <p>Toll Free - 0000 41 9909</p>
+              <p>Email: Info@propdam.com</p>
+              <img className="play-f-img" src={a1} alt="" /> <br />
+              <img className="play-f-img" src={a2} alt="" />
+              <p>
+                All trademarks are the property <br /> of their respective
+                owners.
+              </p>
+              <p>
+                All rights reserved - Secure <br /> Nurture Pvt Ltd.
+              </p>
+            </div>
+            <div className="Subscribe-part">
+              <h3 className="p-h3">Subscribe</h3>
+              <div className="sub-inner">
+                <p>
+                  Subscribe to our newsletter and stay updated on the latest
+                  price and special offers!
+                </p>
+                <input
+                  className="in1"
+                  type="text"
+                  placeholder="Enter Email Address...."
+                />
+                <button className="sub-btn">Subscribe</button>
+              </div>
+            </div>
           </div>
-          <div className="in-yt">
-            <Link>
-              <button className="yt">
-                <FaYoutube />
-              </button>
-            </Link>
-          </div>
         </div>
-      </div>
-      {/* footer inner part */}
-      {/* <div className="inner-info container-fluid">
-        <div className="Propdam-part">
-          <h3 className="p-h3">Propdam</h3>
-          <p>Mobile Apps</p>
-          <p>Our Services</p>
-          <p>Price Trends</p>
-          <p>Builder in India</p>
-          <p>Area Converter</p>
-          <p>Articles</p>
-          <p>Customer Services</p>
-          <p>Sitemap</p>
-        </div>
-        <div className="Company-part">
-          <h3 className="p-h3">Company</h3>
-          <p>About Us</p>
-          <p>Contact Us</p>
-          <p>Carrers with us</p>
-          <p>Terms & Conditions</p>
-          <p>Request Info</p>
-          <p>Feedback</p>
-          <p>Report a problem</p>
-          <p>Testimonials</p>
-          <p>Privacy Policy</p>
-          <p>Summons/Notices</p>
-          <p>Grievances</p>
-          <p>Safety Guide</p>
-        </div>
-        <div className="Contact-us-part">
-          <h3 className="p-h3">Contact Us</h3>
-          <p>Toll Free - 0000 41 9909</p>
-          <p>Email: Info@propdam.com</p>
-          <img className="play-f-img" src={a1} alt="" /> <br />
-          <img className="play-f-img" src={a2} alt="" />
-          <p>
-            All trademarks are the property <br /> of their respective owners.
-          </p>
-          <p>
-            All rights reserved - Secure <br /> Nurture Pvt Ltd.
-          </p>
-        </div>
-        <div className="Subscribe-part">
-          <h3 className="p-h3">Subscribe</h3>
-          <div className="sub-inner">
-            <p>
-              Subscribe to our newsletter and stay updated on the latest price
-              and special offers!
-            </p>
-            <input
-              className="in1"
-              type="text"
-              placeholder="Enter Email Address...."
-            />
-            <button className="sub-btn">Subscribe</button>
-          </div>
-        </div>
-      </div> */}
-    </div>
+      </section>
+    </>
   );
 };
 export default Footer;
